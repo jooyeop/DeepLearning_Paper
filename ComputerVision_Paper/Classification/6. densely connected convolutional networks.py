3c30e088d4e5ed4d958facd51fb0b2beb5f2a60b
JooYeop <97720878+jooyeop@users.noreply.github.com>
2024-04-29 14:59:04 +0900

    DenseNet Implementation

--- ComputerVision_Paper/Classification/6. densely connected convolutional networks.py
+++ ComputerVision_Paper/Classification/6. densely connected convolutional networks.py
@@ -10,6 +10,7 @@ import xml.etree.ElementTree as ET
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 # 이미지 전처리를 위한 transform 설정
 transform = transforms.Compose([
     transforms.Resize(256),
@@ -52,3 +53,97 @@ train_dataset = CustomDataset('../데이터셋/train_zip/train/', '../데이터�
 test_dataset = CustomDataset('../데이터셋/test_zip/test/', '../데이터셋/test_zip/test/', transform=transform)
 train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=64)
+
+model_parameters = {}
+model_parameters['densenet121'] = [6, 12, 24, 16]
+model_parameters['densenet169'] = [6, 12, 32, 32]
+model_parameters['densenet201'] = [6, 12, 48, 32]
+model_parameters['densenet264'] = [6, 12, 64, 48]
+
+# Growth rate
+k = 12
+compression_factor = 0.5
+
+class DenseLayer(nn.Module):
+
+    def __init__(self,in_channels):
+        super(DenseLayer,self).__init__()
+
+        self.BN1 = nn.BatchNorm2d(num_features = in_channels)
+        self.conv1 = nn.Conv2d( in_channels=in_channels , out_channels=4*k , kernel_size=1 , stride=1 , padding=0 , bias = False )
+
+        self.BN2 = nn.BatchNorm2d(num_features = 4*k)
+        self.conv2 = nn.Conv2d( in_channels=4*k , out_channels=k , kernel_size=3 , stride=1 , padding=1 , bias = False )
+
+        self.relu = nn.ReLU()
+
+    def forward(self,x):
+
+        xin = x
+
+        # BN -> relu -> conv(1x1)
+        x = self.BN1(x)
+        x = self.relu(x)
+        x = self.conv1(x)
+
+        # BN -> relu -> conv(3x3)
+        x = self.BN2(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+
+        x = torch.cat([xin,x],1)
+
+        return x
+
+class DenseBlock(nn.Module):
+    def __init__(self,layer_num,in_channels):
+
+        super(DenseBlock,self).__init__()
+        self.layer_num = layer_num
+        self.deep_nn = nn.ModuleList()
+
+        for num in range(self.layer_num):
+            self.deep_nn.add_module(f"DenseLayer_{num}",DenseLayer(in_channels+k*num))
+
+
+    def forward(self,x):
+        xin = x
+        print('xin shape',xin.shape)
+
+        for layer in self.deep_nn:
+            x = layer(x)
+            print('xout shape',x.shape)
+        return x
+
+class TransitionLayer(nn.Module):
+    def __init__(self,in_channels,compression_factor):
+
+        super(TransitionLayer,self).__init__()
+        self.BN = nn.BatchNorm2d(in_channels)
+        self.conv1 = nn.Conv2d(in_channels = in_channels , out_channels = int(in_channels*compression_factor) ,kernel_size = 1 ,stride = 1 ,padding = 0, bias=False )
+        self.avgpool = nn.AvgPool2d(kernel_size = 2, stride = 2)
+
+    def forward(self,x):
+        x = self.BN(x)
+        x = self.conv1(x)
+        x = self.avgpool(x)
+        return x
+
+
+
+class DenseNet(nn.Module):
+    def __init__(self, densenet_variant, in_channels, num_classes = 3):
+        super(DenseNet, self).__init__()
+
+        # 첫 번째 convolution layer 7 * 7 s= 2, maxpooling
+        self.conv1 = nn.Conv2d(in_channels = in_channels, out_channels = 64, kernel_size = 7, stride = 2, padding = 3, bias = False)
+        self.bn1 = nn.BarchNorm2d(num_features = 64)
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size = 2, stride = 2)
+
+        # 3 DenseBlocks and 3 Transition Layers 추가
+        self.deep_nn = nn.ModuleList()
+        dense_block_inchannels = 64
+
+        for num in range(len(densenet_variant))[:-1]:
+            self.deep_nn.add_module(f'DenseBlock_{num+1}', DenseBlock(densenet_variant[num], dense_block_inchannels, k))
